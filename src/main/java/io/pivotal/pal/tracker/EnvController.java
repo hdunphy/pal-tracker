d7b785b6516bb858b0b524d1936df595e357b702
Hdunphy <henry15@gmail.com>
2019-11-04 15:47:58 -0700

    Configuring an App (2) - Lab 2

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,4 +1,46 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class EnvController {
+    /*public void getEnv()throws Exception {
+
+        Map<String, String> env = controller.getEnv();
+
+        assertThat(env.get("PORT")).isEqualTo("8675");
+        assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
+        assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
+        assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
+    }*/
+    private final String PORT, MEMORY_LIMIT, CF_INSTANCE_INDEX, CF_INSTANCE_ADDR;
+
+    public EnvController(
+                         @Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memoryLimit,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr)
+    {
+        PORT = port;
+        MEMORY_LIMIT = memoryLimit;
+        CF_INSTANCE_INDEX = cfInstanceIndex;
+        CF_INSTANCE_ADDR = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", PORT);
+        env.put("MEMORY_LIMIT", MEMORY_LIMIT);
+        env.put("CF_INSTANCE_INDEX", CF_INSTANCE_INDEX);
+        env.put("CF_INSTANCE_ADDR", CF_INSTANCE_ADDR);
+
+        return env;
+    }
 }
